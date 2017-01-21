0fbd48d60763752d2c7672d261ed36b74c1f62be
Luis Muñoz <lrmunoz@gmail.com>
2017-01-21 10:26:56 +0100

    WIP

--- js/main.js
+++ js/main.js
@@ -1,2 +1,12 @@
 $(document).ready(function () {
+  $('button:contains("Generate")').click(function(e) {
+    $("#empty-grid").hide();
+    $("#grid").show();
+  });
+  $('button:contains("Start")').click(function(e) {
+  });
 });
+
+function generateGrid() {
+  console.log();
+}
