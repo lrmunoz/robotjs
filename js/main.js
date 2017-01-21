af88669aac53ac8565fdbb4a01daad70ac600b4f
Luis Muñoz <lrmunoz@gmail.com>
2017-01-22 00:53:44 +0100

    Validate rows and columns input
    Provide validation for the rows/columns input fields

--- js/main.js
+++ js/main.js
@@ -1,12 +1,48 @@
 $(document).ready(function () {
   $('button:contains("Generate")').click(function(e) {
-    $("#empty-grid").hide();
-    $("#grid").show();
+    let validRows = isValidInput($("#rows"));
+    let validColumns = isValidInput($("#columns"));
+    if (validRows && validColumns) {
+      resetInputValidation();
+      $("#empty-grid").hide();
+      $("#grid").show();
+      generateGrid();
+    } else {
+
+    }
   });
   $('button:contains("Start")').click(function(e) {
   });
 });
 
+function isValidInput(element) {
+  if (element.is(":valid")) {
+    $(element).closest(".validation-container").removeClass("has-error");
+    return true;
+  } else {
+    $(element).closest(".validation-container").addClass("has-error");
+    $(".validation-explanation").show();
+    return false;
+  }
+}
+
+function resetInputValidation() {
+  $(".validation-container").removeClass("has-error");
+  $(".validation-explanation").hide();
+}
+
 function generateGrid() {
-  console.log();
+  let rows = $("#rows").val();
+  let columns = $("#columns").val();
+  $("#grid").empty();
+  let grid = document.getElementById("grid");
+  let table = document.createElement("table");
+  grid.appendChild(table);
+  for (var i = 0; i < rows; i++) {
+    let tr = document.createElement("tr");
+    table.appendChild(tr);
+    for (var j = 0; j < columns; j++) {
+      tr.appendChild(document.createElement("td"));
+    }
+  }
 }
